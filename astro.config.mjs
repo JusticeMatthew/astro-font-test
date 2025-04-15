e6bfdd5c36589c77398e46277994a03692470343
Matthew <matthewajustice@gmail.com>
2025-04-15 19:55:13 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,35 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "Calistoga",
+        cssVariable: "--astro-calistoga",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            display: "swap",
+            src: ["./src/assets/fonts/Calistoga.woff2"],
+          },
+        ],
+      },
+      {
+        provider: "local",
+        name: "Inter",
+        cssVariable: "--astro-inter",
+        variants: [
+          {
+            weight: "400 900",
+            style: "normal",
+            display: "swap",
+            src: ["./src/assets/fonts/Inter.woff2"],
+          },
+        ],
+      },
+    ],
+  },
+});
